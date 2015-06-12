cbd74ab692acdf4100bcbddab633e73835666069
jon-xavier <jonathan.xavier0@gmail.com>
2015-06-12 03:48:20 +0000

    added posting forms

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from blog import app
 from .database import session
 from .models import Post
+import mistune
+
 
 @app.route("/")
 @app.route("/page/<int:page>")
@@ -30,3 +32,17 @@ def posts(page=1, paginate_by=10):
         page=page,
         total_pages=total_pages
     )
+
+@app.route("/post/add", methods=["GET"])
+def add_post_get():
+    return render_template("add_post.html")
+
+@app.route("/post/add", methods=["POST"])
+def add_post_post():
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
