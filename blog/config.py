f87b5272ef0131ff3cd459976318ad290450c9e4
jon-xavier <jonathan.xavier0@gmail.com>
2015-06-18 00:29:54 +0000

    added WYSIWYG editor, truncated posts on front page

--- blog/config.py
+++ blog/config.py
@@ -2,4 +2,4 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
     DEBUG = True
-    SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
+    SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "secret")
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+from flask.ext.login import login_user, login_required
+from werkzeug.security import check_password_hash
 
 from blog import app
+
 from .database import session
-from .models import Post
+from .models import Post, User
 import mistune
 
 
@@ -34,10 +37,12 @@ def posts(page=1, paginate_by=10):
     )
 
 @app.route("/post/add", methods=["GET"])
+##@login_required
 def add_post_get():
     return render_template("add_post.html")
 
 @app.route("/post/add", methods=["POST"])
+##s@login_required
 def add_post_post():
     post = Post(
         title=request.form["title"],
@@ -53,14 +58,16 @@ def individual_post_view(post_id):
        post=session.query(Post).get(post_id)
     )
     
-@app.route("/post/<post_id>/edit", methods=["GET"])
+@app.route("/post/<post_id>/edit/", methods=["GET"])
+##@login_required
 def edit_post_get(post_id):
     post=session.query(Post).get(post_id)
     body = post.content
     return render_template("edit_post.html", post=post, body=body)
                         
     
-@app.route("/post/<post_id>/edit", methods=["POST"])
+@app.route("/post/<post_id>/edit/", methods=["POST"])
+##@login_required
 def edit_post_post(post_id):
     post=session.query(Post).get(post_id)
     post.title = request.form["title"]
@@ -70,6 +77,7 @@ def edit_post_post(post_id):
     return redirect(url_for("posts"))
 
 @app.route("/post/<post_id>/delete/", methods=["GET"])
+##@login_required
 def delete_post(post_id):
     post=session.query(Post).get(post_id)
     session.delete(post)
@@ -77,3 +85,18 @@ def delete_post(post_id):
     
     return redirect(url_for("posts"))
 
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("posts"))
